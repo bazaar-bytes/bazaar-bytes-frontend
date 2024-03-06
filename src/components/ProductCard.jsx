f643efb402f5d7d70d06b71a1a618745fd21ea82
Marianna Di Vito <maridivi89@gmail.com>
2024-03-06 10:57:14 +0000

    Install daisyUI + basic style for product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,17 @@
 export const ProductCard = ({ product }) => {
-  const { name } = product;
+  const { name, image, description } = product;
   return (
-    <div>
-      <h2>{name}</h2>
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt="play-station" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
     </div>
   );
 };
