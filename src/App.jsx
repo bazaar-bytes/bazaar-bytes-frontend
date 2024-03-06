616655229dfe657347a775b3f9d1866b243cf9cd
Marianna Di Vito <maridivi89@gmail.com>
2024-03-06 21:51:06 +0000

    Add more routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import "./App.css";
+import { AddProductPage } from "./pages/AddProductPage";
 import { ProductListPage } from "./pages/ProductListPage";
 import { Routes, Route } from "react-router-dom";
 import { SignupPage } from "./pages/SignupPage";
 import { LoginPage } from "./pages/LoginPage";
+import { ProductDetailsPage } from "./pages/ProductDetailsPage";
+import { Navbar } from "./components/Navbar";
 import { EditProductPage } from "./pages/EditProductPage";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<ProductListPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="p-10">
+        <Routes>
+          <Route path="/" element={<ProductListPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/products/add" element={<AddProductPage />} />
+          <Route
+            path="/products/details/:productId"
+            element={<ProductDetailsPage />}
+          />
+        </Routes>
         <Route path="/products/:productId" element={<EditProductPage />} />
-      </Routes>
-    </>
+      </div>
+    </div>
   );
 }
 
