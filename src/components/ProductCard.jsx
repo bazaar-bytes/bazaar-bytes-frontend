49400961645f7b9a6aca541be7585993c1007737
Marianna Di Vito <maridivi89@gmail.com>
2024-03-06 21:51:17 +0000

    Work on style

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export const ProductCard = ({ product }) => {
-  const { name, image, description } = product;
+  const { _id, name, image, price } = product;
+
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src={image} alt="play-station" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+    <Link to={`/products/details/${_id}`} className="mx-auto">
+      <div className="card w-full h-full bg-base-100 shadow-xl">
+        <figure>
+          <img src={image} alt={name} className="object-cover w-80 h-72" />
+        </figure>
+        <div className="card-body text-left">
+          <h2 className="card-title">{name}</h2>
+          <p>{price}</p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { ProductCard } from "../components/ProductCard";
+import { Link } from "react-router-dom";
 
 export const ProductListPage = () => {
   const API_URL = "http://localhost:5005";
@@ -9,20 +10,24 @@ export const ProductListPage = () => {
 
   useEffect(() => {
     axios.get(`${API_URL}/api/products`).then((response) => {
-      console.log(response);
       setProducts(response.data);
     });
   }, []);
-  console.log(products);
+
   return (
     <div>
-      {products === null ? (
-        <h1>Loading...</h1>
-      ) : (
-        products.map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))
-      )}
+      <Link to="/products/add">
+        <button className="btn btn-primary">Add New Product</button>
+      </Link>
+      <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {products === null ? (
+          <h1>Loading...</h1>
+        ) : (
+          products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))
+        )}
+      </div>
     </div>
   );
 };
