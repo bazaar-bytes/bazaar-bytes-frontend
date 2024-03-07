f10e70169c70fd112ff9a7a7e979e23ab69d9c6f
EazyErik <erik_busch@gmx.de>
2024-03-07 09:47:08 +0100

    implement add to shopping cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,27 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export const ProductCard = ({ product }) => {
   const { name, image, description } = product;
+  const navigate = useNavigate();
+
+  const API_URL = `http://localhost:5005`;
+
+  const handleBuyClick = () => {
+    const token = localStorage.getItem("auth");
+    axios
+      .post(`${API_URL}/api/orders`, product, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response);
+        navigate("/buy");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -9,7 +31,9 @@ export const ProductCard = ({ product }) => {
         <h2 className="card-title">{name}</h2>
         <p>{description}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button onClick={handleBuyClick} className="btn btn-primary">
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
