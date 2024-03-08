6f053b4cc9496d9bd43055163d0d08d6f42d292a
Marianna Di Vito <maridivi89@gmail.com>
2024-03-08 16:42:16 +0000

    Move buy button to product details page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,43 +1,21 @@
-import axios from "axios";
-import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/auth.context";
+import { Link } from "react-router-dom";
 
 export const ProductCard = ({ product }) => {
   const { _id, name, image, price } = product;
-  const productId = product._id;
-
-  const { user } = useContext(AuthContext);
-
-  const API_URL = "http://localhost:5005";
-
-  const handleBuyClick = () => {
-    const token = localStorage.getItem("authToken");
-    axios
-      .post(
-        `${API_URL}/api/cart`,
-        { product: productId, user: user._id },
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      )
-      .then((response) => {})
-      .catch((error) => console.error(error));
-  };
 
   return (
-    <Link to={`/products/details/${_id}`} className="mx-auto">
-      <div className="card w-full h-full bg-base-100 shadow-xl">
-        <figure>
-          <img src={image} alt={name} className="object-cover w-80 h-72" />
+    <Link to={`/products/details/${_id}`} className="mx-auto ">
+      <div className="card w-full h-72 bg-base-100">
+        <figure className="rounded-md shadow-md">
+          <img src={image} alt={name} className="object-cover w-full h-full" />
         </figure>
-        <div className="card-body text-left">
-          <h2 className="card-title">{name}</h2>
-          <p>{price}</p>
+        <div className="card-body text-left px-4 py-2 ">
+          <h3 className="card-title text-sm leading-none">{name}</h3>
+          <p className="text-sm leading-none">€{price}</p>
+          <p className="text-sm leading-none">
+            Ad by {product.createdBy?.name}
+          </p>
         </div>
-        <Link to={`/products/details/${_id}/buy`}>
-          <button onClick={handleBuyClick}>Buy</button>
-        </Link>
       </div>
     </Link>
   );
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,51 +1,75 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 const API_URL = "http://localhost:5005";
 
 export const ProductDetailsPage = () => {
   const { isLoggedIn, user } = useContext(AuthContext);
   const [product, setProduct] = useState(null);
-
-  const isOwner =
-    product !== null &&
-    user !== null &&
-    product.createdBy.toString() === user._id.toString();
-
-  console.log(isOwner);
+  const [isOwner, setIsOwner] = useState(false);
 
   const { productId } = useParams();
+  console.log(product);
+
+  const handleBuyClick = () => {
+    const token = localStorage.getItem("authToken");
+    axios
+      .post(
+        `${API_URL}/api/cart`,
+        { product: productId, user: user._id },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.error(error));
+  };
 
   useEffect(() => {
-    axios
-      .get(`${API_URL}/api/products/${productId}`)
-      .then((response) => {
-        setProduct(response.data);
-      })
-      .catch((error) => console.log(error));
-  }, [productId]);
+    if (user) {
+      axios
+        .get(`${API_URL}/api/products/${productId}`)
+        .then((response) => {
+          setProduct(response.data);
+          setIsOwner(response.data.createdBy === user._id);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [user, productId]);
 
   return (
     <div>
       {product === null ? (
         <h1>Loading...</h1>
       ) : (
-        <div className="card lg:card-side bg-base-100   md:w-2/3 xl:w-1/2 mx-auto">
-          <figure>
-            <img src={product.image} alt={product.name} />
+        <div className="card lg:card-side bg-base-100 rounded-none xl:w-2/3 mx-auto ">
+          <figure className="h-80 sm:h-[550px] lg:w-1/2">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="object-cover"
+            />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">{product.name}</h2>
+          <div className="card-body text-left">
+            <h2 className="card-title ">{product.name}</h2>
             <p>{product.description}</p>
-            <div className="card-actions justify-end">
+            <p>€{product.price}</p>
+            <div className="card-actions ">
               {isOwner ? (
                 <Link to={`/products/edit/${productId}`}>
-                  <button className="btn btn-primary">Edit</button>
+                  <button className="btn btn-primary bg-indigo-600">
+                    Edit
+                  </button>
                 </Link>
               ) : (
-                <Link>
-                  <button className="btn btn-primary">Buy</button>
+                <Link to={`/cart`}>
+                  <button className="btn btn-primary" onClick={handleBuyClick}>
+                    Buy
+                  </button>
                 </Link>
               )}
             </div>
