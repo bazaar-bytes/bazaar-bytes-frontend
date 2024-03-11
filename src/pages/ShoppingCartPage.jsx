c18fcb7460363c3c8e3f135bd4d2c93f097c61fa
Marianna Di Vito <maridivi89@gmail.com>
2024-03-11 14:47:06 +0000

    Make cart page responsive

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -1,10 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { API_URL } from "./ProductListPage";
+import { CartContext } from "../context/cart.context";
+import { calculateSubtotal } from "../utils/calculateSubtotal";
+import { MobileCart } from "../components/MobileCart";
 
 export const ShoppingCartPage = () => {
-  const [cartItems, setCartItems] = useState();
+  const { cartItems, setCartItems } = useContext(CartContext);
+  console.log(cartItems);
   const token = localStorage.getItem("authToken");
 
   const shipping = 20;
@@ -40,16 +44,16 @@ export const ShoppingCartPage = () => {
     });
   };
 
-  const calculateSubtotal = () => {
-    if (!cartItems) return 0;
-    return cartItems.reduce((total, item) => {
-      return total + item.product.price * item.quantity;
-    }, 0);
-  };
+  // const calculateSubtotal = () => {
+  //   if (!cartItems) return 0;
+  //   return cartItems.reduce((total, item) => {
+  //     return total + item.product?.price * item.quantity;
+  //   }, 0);
+  // };
 
   const calculateTotal = () => {
     if (!cartItems) return 0;
-    const subtotal = calculateSubtotal();
+    const subtotal = calculateSubtotal(cartItems);
 
     return subtotal > 0 ? subtotal + shipping : subtotal;
   };
@@ -94,9 +98,9 @@ export const ShoppingCartPage = () => {
   }
 
   return (
-    <div className="bg-gray-100 py-8">
+    <div className="bg-gray-100 py-4">
       <div className="container mx-auto px-4">
-        <div className="flex justify-between border-b pb-8">
+        <div className="flex justify-between border-b pb-4">
           <h1 className="font-semibold text-2xl text-center">Shopping Cart</h1>
           <h2 className="font-semibold text-2xl">
             {cartItems && cartItems.length} Items
@@ -104,9 +108,9 @@ export const ShoppingCartPage = () => {
         </div>
         <div className="flex flex-col md:flex-row gap-4">
           <div className="md:w-3/4">
-            <div className="bg-white rounded-lg shadow-md p-6 mb-4">
-              <table className="w-full">
-                <thead>
+            <div className="bg-white rounded-lg shadow-md p-6 mb-4 hidden sm:block">
+              <table className="w-full ">
+                <thead className="">
                   <tr>
                     <th className="text-center font-semibold ">Product</th>
                     <th className="text-center font-semibold ">Price</th>
@@ -118,49 +122,48 @@ export const ShoppingCartPage = () => {
                   {cartItems &&
                     cartItems.map((item, index) => {
                       return (
-                        <tr key={item._id}>
+                        <tr key={item?._id}>
                           <td className="py-4 border-t-4 ">
                             <div className="flex justify-between items-center">
                               <img
-                                className="h-16 w-16 mr-4"
-                                src={item.product.image}
+                                className="h-16 w-16"
+                                src={item.product?.image}
                                 alt="Product image"
                               />
                               <span className="font-semibold">
-                                {item.product.name}
+                                {item.product?.name}
                               </span>
 
-                              <span className="">
-                                {""}
-
-                                <button
-                                  className=""
-                                  onClick={() => handleDeleteClick(item)}
+                              <button
+                                className=" mb-1"
+                                onClick={() => handleDeleteClick(item)}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="w-6 h-6"
                                 >
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-6 h-6"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                    />
-                                  </svg>
-                                </button>
-                              </span>
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                  />
+                                </svg>
+                              </button>
                             </div>
                           </td>
-                          <td className="py-4">${item.product.price}</td>
+                          <td className="py-4">${item.product?.price}</td>
                           <td className="py-4">
-                            <div className="flex items-center">
+                            <div
+                              className="flex items-center justify-center 
+                             w-max mx-auto"
+                            >
                               <button
                                 onClick={() => handleDecrementClick(index)}
-                                className="border rounded-md py-2 px-4 mr-2"
+                                className="border rounded-md py-2 px-4"
                               >
                                 -
                               </button>
@@ -169,14 +172,14 @@ export const ShoppingCartPage = () => {
                               </span>
                               <button
                                 onClick={() => handleIncrementClick(index)}
-                                className="border rounded-md py-2 px-4 ml-2"
+                                className="border rounded-md py-2 px-4"
                               >
                                 +
                               </button>
                             </div>
                           </td>
                           <td className="py-4">
-                            ${item.quantity * item.product.price}
+                            ${item.quantity * item.product?.price}
                           </td>
                         </tr>
                       );
@@ -184,13 +187,19 @@ export const ShoppingCartPage = () => {
                 </tbody>
               </table>
             </div>
+            <MobileCart
+              cartItems={cartItems}
+              handleDecrementClick={handleDecrementClick}
+              handleDeleteClick={handleDeleteClick}
+              handleIncrementClick={handleIncrementClick}
+            />
           </div>
           <div className="md:w-1/4">
             <div className="bg-white rounded-lg shadow-md p-6">
               <h2 className="text-lg font-semibold mb-4">Summary</h2>
               <div className="flex justify-between mb-2">
                 <span>Subtotal</span>
-                <span>${calculateSubtotal()}</span>
+                <span>${calculateSubtotal(cartItems).toFixed(2)}</span>
               </div>
               <div className="flex justify-between mb-2">
                 <span>Shipping</span>
@@ -199,7 +208,9 @@ export const ShoppingCartPage = () => {
               <hr className="my-2" />
               <div className="flex justify-between mb-2">
                 <span className="font-semibold">Total</span>
-                <span className="font-semibold">${calculateTotal()}</span>
+                <span className="font-semibold">
+                  ${calculateTotal().toFixed(2)}
+                </span>
               </div>
 
               {cartItems && cartItems.length > 0 && (
