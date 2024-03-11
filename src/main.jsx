85e4c6298725e8ba14899cea17b85f34b81aa754
Marianna Di Vito <maridivi89@gmail.com>
2024-03-11 14:46:34 +0000

    Add cart icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProviderWrapper } from "./context/auth.context.jsx";
-import {
-  ProductsContext,
-  ProductsProviderWrapper,
-} from "./context/products.context.jsx";
+import { ProductsProviderWrapper } from "./context/products.context.jsx";
+import { CartProviderWrapper } from "./context/cart.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ProductsProviderWrapper>
-      <AuthProviderWrapper>
-        <App />
-      </AuthProviderWrapper>
-    </ProductsProviderWrapper>
+    <AuthProviderWrapper>
+      <ProductsProviderWrapper>
+        <CartProviderWrapper>
+          <App />
+        </CartProviderWrapper>
+      </ProductsProviderWrapper>
+    </AuthProviderWrapper>
   </BrowserRouter>
 );
