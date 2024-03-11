5da82b070c349adf9c84449d7d945a161e6aa1f5
Marianna Di Vito <maridivi89@gmail.com>
2024-03-11 09:32:17 +0000

    Show creator name only for public products

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export const ProductCard = ({ product }) => {
+export const ProductCard = ({ product, isPublic }) => {
   const { _id, name, image, price } = product;
 
   return (
@@ -12,9 +12,11 @@ export const ProductCard = ({ product }) => {
         <div className="card-body text-left px-4 py-2 ">
           <h3 className="card-title text-sm leading-none">{name}</h3>
           <p className="text-sm leading-none">€{price}</p>
-          <p className="text-sm leading-none">
-            Ad by {product.createdBy?.name}
-          </p>
+          {isPublic && (
+            <p className="text-sm leading-none">
+              Ad by {product.createdBy?.name}
+            </p>
+          )}
         </div>
       </div>
     </Link>
