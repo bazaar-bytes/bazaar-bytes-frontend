67f7d66a9be3db952fd3e8c5632325982e150239
Marianna Di Vito <maridivi89@gmail.com>
2024-03-11 14:45:50 +0000

    Use useDebounce hook to limit API calls when searching

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,36 +2,34 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../context/products.context";
 import { API_URL } from "../pages/ProductListPage";
+import { useDebounce } from "use-debounce";
 
 export const SearchBar = () => {
   const [query, setQuery] = useState("");
+  const [debouncedQuery] = useDebounce(query, 1000, { leading: false });
   const { setProducts } = useContext(ProductsContext);
 
   useEffect(() => {
-    console.log(query);
     axios
-      .get(`${API_URL}/api/products/search?q=${query}`)
+      .get(`${API_URL}/api/products/search?q=${debouncedQuery}`)
       .then((response) => {
-        console.log(response.data);
         setProducts(response.data);
       })
       .catch((e) => {
         console.log("Error getting products from the API...");
         console.error(e);
       });
-  }, [setProducts, query]);
+  }, [setProducts, debouncedQuery]);
 
   return (
-    <div>
-      <div className="form-control">
-        <input
-          type="text"
-          placeholder="Search"
-          className="input input-bordered w-24 md:w-auto h-10 rounded-full text-black"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-      </div>
+    <div className="form-control">
+      <input
+        type="text"
+        placeholder="Search"
+        className="input input-bordered w-24 md:w-auto h-10 rounded-full text-black"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
     </div>
   );
 };
