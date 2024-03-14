14a812159c143cb70ffea03d6e5cf935c51a07c3
Marianna Di Vito <maridivi89@gmail.com>
2024-03-14 14:48:00 +0000

    Work on style

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -47,15 +47,15 @@ export const ProductDetailsPage = () => {
       {product === null ? (
         <span className="loading loading-spinner loading-lg"></span>
       ) : (
-        <div className="card md:flex-row  bg-base-100 rounded-none xl:w-2/3 mx-auto items-center gap-8">
-          <figure className="aspect-square w-[300px] md:w-[700px] lg:w-1/2">
+        <div className="card md:flex-row  bg-base-100 rounded-none xl:w-2/3 mx-auto items-center gap-8 md:gap-16 lg:gap-24 xl:gap-32 ">
+          <figure className="aspect-square w-full md:w-1/2 lg:w-1/2 ">
             <img
               src={product.image}
               alt={product.name}
-              className="object-cover"
+              className="object-cover "
             />
           </figure>
-          <div className=" text-left flex flex-col gap-4">
+          <div className="text-left flex flex-col gap-4  w-full md:w-1/2  ">
             <h2 className="card-title ">{product.name}</h2>
             <p>{product.description}</p>
             <p className="font-bold">€{product.price}</p>
@@ -68,8 +68,11 @@ export const ProductDetailsPage = () => {
                 </Link>
               ) : (
                 <Link to={`/cart`}>
-                  <button className="btn btn-primary" onClick={handleBuyClick}>
-                    Buy
+                  <button
+                    className="btn btn-primary rounded-2xl text-lg"
+                    onClick={handleBuyClick}
+                  >
+                    Add to cart
                   </button>
                 </Link>
               )}
