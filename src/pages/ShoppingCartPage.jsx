44c93cf0ad3feb5409abc00f1e8202611b09236c
Marianna Di Vito <maridivi89@gmail.com>
2024-03-16 16:31:55 +0000

    Fix cart error and responsiveness

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -2,7 +2,6 @@ import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { CartContext } from "../context/cart.context";
-import { MobileCart } from "../components/MobileCart";
 
 export const ShoppingCartPage = () => {
   const { cartItems, fetchCartItems } = useContext(CartContext);
@@ -98,98 +97,75 @@ export const ShoppingCartPage = () => {
   }
 
   return (
-    <div className="bg-gray-100 py-4 flex flex-col gap-8 px-8">
+    <div className="bg-gray-100 py-4 flex flex-col gap-8 px-4">
       <div className="container max-w-full ">
         <div className="flex justify-between border-b pb-4">
           <h1 className="font-semibold text-2xl text-center">Shopping Cart</h1>
         </div>
         <div className="flex flex-col min-[900px]:flex-row justify-between gap-8">
           <div className="min-[900px]:w-[70%]">
-            <div className="bg-white rounded-lg shadow-md p-6 mb-4 hidden sm:block w-full">
-              <table className="w-full ">
-                <thead className="">
-                  <tr>
-                    <th className="text-center font-semibold ">Product</th>
-                    <th className="text-center font-semibold ">Price</th>
-                    <th className="text-center font-semibold ">Quantity</th>
-                    <th className="text-center font-semibold ">Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {cartItems &&
-                    cartItems.map((item) => {
-                      return (
-                        <tr key={item?._id}>
-                          <td className="py-4 border-t-4 ">
-                            <div className="flex justify-between items-center">
-                              <img
-                                className="h-16 w-16 mr-4"
-                                src={item?.product?.image}
-                                alt="Product image"
-                              />
-                              <span className="font-semibold">
-                                {item.product?.name}
-                              </span>
+            <div className="bg-white rounded-lg shadow-md p-6 mb-4  sm:block w-full">
+              <div className="flex flex-col gap-10">
+                {cartItems &&
+                  cartItems.map((item) => {
+                    return (
+                      <div
+                        key={item?._id}
+                        className="flex flex-col gap-3 min-[550px]:flex-row min-[550px]:justify-between"
+                      >
+                        <div className="flex  items-center justify-between min-[550px]:gap-6 sm:gap-16 md:gap-24 min-[900px]:gap-16 lg:gap-24 xl:gap-32">
+                          <img
+                            className="h-20 w-20"
+                            src={item?.product?.image}
+                            alt="Product image"
+                          />
 
-                              <button
-                                className=" mb-1"
-                                onClick={() => handleDeleteClick(item)}
-                              >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth={1.5}
-                                  stroke="currentColor"
-                                  className="w-6 h-6"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                  />
-                                </svg>
-                              </button>
-                            </div>
-                          </td>
-                          <td className="py-4">${item.product?.price}</td>
-                          <td className="py-4">
-                            <div
-                              className="flex items-center justify-center 
-                             w-max mx-auto"
+                          <div className="flex flex-col items-start">
+                            <h3>{item.product?.name}</h3>
+                            <p className="font-bold">${item.product?.price}</p>
+                          </div>
+                          <button onClick={() => handleDeleteClick(item)}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-6 h-6"
                             >
-                              <button
-                                onClick={() => handleDecrementClick(item)}
-                                className="border rounded-md py-2 px-4 mr-2"
-                              >
-                                -
-                              </button>
-                              <span className="text-center w-8">
-                                {item.quantity}
-                              </span>
-                              <button
-                                onClick={() => handleIncrementClick(item)}
-                                className="border rounded-md py-2 px-4 ml-2"
-                              >
-                                +
-                              </button>
-                            </div>
-                          </td>
-                          <td className="py-4">
-                            ${item.quantity * Math.round(item.product?.price)}
-                          </td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                        <div
+                          className="flex items-center 
+                             w-max"
+                        >
+                          <button
+                            onClick={() => handleDecrementClick(item)}
+                            className="border rounded-md py-2 px-4 mr-2"
+                          >
+                            -
+                          </button>
+                          <span className="text-center w-8">
+                            {item.quantity}
+                          </span>
+                          <button
+                            onClick={() => handleIncrementClick(item)}
+                            className="border rounded-md py-2 px-4 ml-2"
+                          >
+                            +
+                          </button>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
             </div>
-            <MobileCart
-              cartItems={cartItems}
-              handleDecrementClick={handleDecrementClick}
-              handleDeleteClick={handleDeleteClick}
-              handleIncrementClick={handleIncrementClick}
-            />
           </div>
           <div className="min-[900px]:w-1/4">
             <div className="bg-white rounded-lg shadow-md p-6">
